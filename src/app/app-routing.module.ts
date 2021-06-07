acbc39b48c3b34188a7449fe9f2fcd46b9b91848
gauravkingrani <85162765+gauravkingrani@users.noreply.github.com>
2021-06-07 20:28:56 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,9 @@ import { AdminComponent } from './admin/admin.component';
 
 
 const routes: Routes = [
-  {path: 'home',loadChildren:()=>import('./signin/lazy/lazy.module').then(m=> m.LazyModule),canActivate:[AuthGuard]},
+  {path: '',loadChildren:()=>import('./signin/lazy/lazy.module').then(m=> m.LazyModule),canActivate:[AuthGuard]},
   {path: 'login',component:SigninComponent},
-  {path: 'admin',component:AdminComponent},
-    {path: '', redirectTo: '/login',pathMatch: 'full'}
+  {path: 'admin',component:AdminComponent}
 ];
 
 @NgModule({
